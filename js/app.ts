33c1dfb7582dc97fa36156e4dd98880755891aca
Olga <klokova.plus@gmail.com>
2021-12-21 01:05:36 +0100

    feat: add class, btn

--- js/app.ts
+++ js/app.ts
@@ -1,127 +1,33 @@
 import "../sass/style.scss";
 import data from '../assets/data.js';
+import Card from '../js/card'
 
 const page = document.querySelector(".page");
 const cardContauner: HTMLElement = document.querySelector(".card-container") as HTMLElement;
-let favotiteNum: any = {};
 
-class Card {
-    num: string;
-    name: string;
-    count: string;
-    year: string;
-    shape: string;
-    color: string;
-    size: string;
-    favorite: boolean;
 
-    constructor({ num, name, count, year, shape, color, size, favorite }: { num: string; name: string; count: string; year: string; shape: string; color: string; size: string; favorite: boolean; }) {
-        this.num = num;
-        this.name = name;
-        this.count = count;
-        this.year = year;
-        this.shape = shape;
-        this.color = color;
-        this.size = size;
-        this.favorite = favorite;
-    }
-    toggleFavorit(){
-        this.favorite = !this.favorite
-        return this.favorite;
-    }
+const home = document.querySelector('#home');
+const tous = document.querySelector('#tous');
+const titleBtn = document.querySelector('#title-btn');
+const tree = document.querySelector('#tree');
+const mainePage = document.querySelector('#main-page');
+const homePage = document.querySelector('#start-page');
+home.addEventListener('click', () => { startPage()});
+titleBtn.addEventListener('click', () => {  tousPage()});
+tous.addEventListener('click', () => { tousPage()});
+
+
+let cardX =  data.forEach(item => {
+    let cardX = new Card (item);
+    cardX.render();
+});
+
+function startPage(): void {
+    mainePage.className = 'none';
+    homePage.className = 'page start-page';
 }
 
-// const createImage = (src: string) => new Promise((res, rej) => {
-//     const img = new Image();
-//     img.onload = () => res(img);
-//     img.onerror = rej;
-//     img.src = src;
-//   });
-
-// let cardX = new Card ({ name: data[0].name, size: data[0].size, favorite: data[0].favorite })
-// console.log(cardX);
-
-data.forEach(item => {
-    let cardX = new Card ({ num: item.num, name: item.name, count: item.count, year: item.year, shape: item.shape, color: item.color, size: item.size, favorite: item.favorite })
-    let card = document.createElement('div');
-    card.className = 'card';
-    card.setAttribute(`data-num`, `${cardX.num}`);
-    cardContauner.appendChild(card);
-
-    let cardTitle = document.createElement('h2');
-    cardTitle.className = 'card-title';
-    cardTitle.innerHTML = cardX.name; 
-    card.appendChild(cardTitle);
-    
-
-    let cardImg = new Image();
-    cardImg.className = 'card-img';
-    cardImg.src = `../assets/toys/${cardX.num}.webp`
-    card.appendChild(cardImg);
-
-
-    let cardDescription = document.createElement('div');
-    cardDescription.className = 'card-description';
-    card.appendChild(cardDescription);
-
-    let cardCount = document.createElement('p');
-    cardCount.className = `count`;
-    cardCount.innerHTML = `Kолличество: ${cardX.count}`; 
-    cardDescription.appendChild(cardCount);
-
-    let cardYear = document.createElement('p');
-    cardYear.className = `year`;
-    cardYear.innerHTML = `Год: ${cardX.year}`; 
-    cardDescription.appendChild(cardYear);
-
-    let cardShape = document.createElement('p');
-    cardShape.className = `shape`;
-    cardShape.innerHTML = `Форма: ${cardX.shape}`; 
-    cardDescription.appendChild(cardShape);
-
-    let cardColor = document.createElement('p');
-    cardColor.className = `color`;
-    cardColor.innerHTML = `Цвет: ${cardX.color}`; 
-    cardDescription.appendChild(cardColor);
-
-    let cardSize = document.createElement('p');
-    cardSize.className = `size`;
-    cardSize.innerHTML = `Размер: ${cardX.size}`; 
-    cardDescription.appendChild(cardSize);
-
-    let cardFavor= document.createElement('p');
-    cardFavor.className = `favorite`;
-    let yes = 'да';
-    let no = 'нет';
-    if (cardX.favorite === false) {
-        cardFavor.innerHTML = `Любимая: ${no}`; 
-    } else
-    if (cardX.favorite === true) {
-        cardFavor.innerHTML = `Любимая: ${yes}`; 
-    }
-    
-    cardDescription.appendChild(cardFavor);
-    let cardRibbon = document.createElement('div');
-   
-    cardRibbon.className = 'ribbon';
-    if (cardX.favorite === true) {
-        card.classList.add('active')
-        cardRibbon.classList.add('active')
-    } else {
-        card.className = ('card');
-    }
-    card.appendChild(cardRibbon);
-})
-
-
-cardContauner.onclick = function(event: any) { //question???
-    console.log(event.target.dataset);
-    if (event.target.className === 'card') {
-        event.target.classList.add('active');
-    } else if (event.target.className === 'card active')
-    {
-        event.target.classList.remove('active');
-
-    }
-    return;
+function tousPage(): void {
+    homePage.className = 'none';
+    mainePage.className = 'page main-page';
 }
---
+++ js/card.ts
@@ -0,0 +1,133 @@
+import "../sass/style.scss";
+import data from '../assets/data.js';
+
+const page = document.querySelector(".page");
+const cardContauner: HTMLElement = document.querySelector(".card-container") as HTMLElement;
+
+export class Card {
+    num: string;
+    name: string;
+    count: string;
+    year: string;
+    shape: string;
+    color: string;
+    size: string;
+    favorite: boolean;
+
+    private cardFavor: HTMLParagraphElement;
+    private cardRibbon: HTMLDivElement;
+    private card: HTMLDivElement;
+
+    constructor({ num, name, count, year, shape, color, size, favorite }: { num: string; name: string; count: string; year: string; shape: string; color: string; size: string; favorite: boolean; }) {
+        this.num = num;
+        this.name = name;
+        this.count = count;
+        this.year = year;
+        this.shape = shape;
+        this.color = color;
+        this.size = size;
+        this.favorite = favorite;
+    }
+
+    toggleFavorite() {
+        this.favorite = !this.favorite
+        return this.favorite;
+    }
+
+    render(): void {
+        // TODO add rendering
+        this.card = document.createElement('div');
+        this.card.className = 'card';
+        this.card.setAttribute(`data-num`, `${this.num}`);
+        cardContauner.appendChild(this.card);
+
+        let cardTitle = document.createElement('h2');
+        cardTitle.className = 'card-title';
+        cardTitle.innerHTML = this.name; 
+        this.card.appendChild(cardTitle);
+
+        let cardImg = new Image();
+        cardImg.className = 'card-img';
+        cardImg.src = `../assets/toys/${this.num}.webp`
+        this.card.appendChild(cardImg);
+
+        let cardDescription = document.createElement('div');
+        cardDescription.className = 'card-description';
+        this.card.appendChild(cardDescription);
+
+        let cardCount = document.createElement('p');
+        cardCount.className = `count`;
+        cardCount.innerHTML = `Kолличество: ${this.count}`; 
+        cardDescription.appendChild(cardCount);
+
+        let cardYear = document.createElement('p');
+        cardYear.className = `year`;
+        cardYear.innerHTML = `Год: ${this.year}`; 
+        cardDescription.appendChild(cardYear);
+
+        let cardShape = document.createElement('p');
+        cardShape.className = `shape`;
+        cardShape.innerHTML = `Форма: ${this.shape}`; 
+        cardDescription.appendChild(cardShape);
+
+        let cardColor = document.createElement('p');
+        cardColor.className = `color`;
+        cardColor.innerHTML = `Цвет: ${this.color}`; 
+        cardDescription.appendChild(cardColor);
+
+        let cardSize = document.createElement('p');
+        cardSize.className = `size`;
+        cardSize.innerHTML = `Размер: ${this.size}`; 
+        cardDescription.appendChild(cardSize);
+
+
+        this.cardFavor= document.createElement('p');
+
+        this.cardFavor.className = `favorite`;
+        let yes = 'да';
+        let no = 'нет';
+        if (this.favorite === false) {
+            this.cardFavor.innerHTML = `Любимая: ${no}`; 
+        } else
+        if (this.favorite === true) {
+            this.cardFavor.innerHTML = `Любимая: ${yes}`; 
+        }
+        
+        cardDescription.appendChild(this.cardFavor);
+
+        this.cardRibbon = document.createElement('div');
+       
+        this.cardRibbon.className = 'ribbon';
+        if (this.favorite === true) {
+            this.card.classList.add('active')
+            this.cardRibbon.classList.add('active')
+        } else {
+            this.card.className = ('card');
+        }
+        this.card.appendChild(this.cardRibbon);
+
+        this.card.onclick = this.clicked.bind(this);
+    }
+
+    clicked(): void {
+        console.log(this.card);
+        if(this.favorite === false){
+            this.favorite = this.toggleFavorite();
+            this.cardFavor.innerText= 'Любимая: да';
+            this.cardRibbon.classList.add('active')
+            this.card.classList.add('active')
+        } else  if(this.favorite === true){
+            this.favorite = this.toggleFavorite();
+            this.cardFavor.innerText= 'Любимая: нет';
+            this.cardRibbon.classList.remove('active');
+            this.card.classList.remove('active')
+        } 
+    }
+}
+
+export default Card;
+
+// let cardX =  data.forEach(item => {
+//     let cardX = new Card (item);
+//     cardX.render();
+// });
