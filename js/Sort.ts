df8fce49a246e853a9bf71ae5b2a973eb2d8f295
Olga <klokova.plus@gmail.com>
2022-01-13 21:34:50 +0100

    fix

--- js/Sort.ts
+++ js/Sort.ts
@@ -1,13 +1,9 @@
-import { Filter } from './interfaces';
-import { Card } from "./card";
 const cardContauner: HTMLElement = document.querySelector(".card-container") as HTMLElement;
 const filtersSort: HTMLDivElement = document.querySelector('.filters-sort');
-
 export class FiltersSort {
     sortSelect: any;
     callback: () => void;
     filterCards: () => void;
-    
 
     constructor(callback: () => void, filterCards: () => void) {
         this.callback = callback;
@@ -17,12 +13,10 @@ export class FiltersSort {
 
     checkSortNameIsSelected(a: string, b: string): number {
         if (this.sortSelect.value === 'sort-name-max'){
-            console.log(a.localeCompare(b))
-            return a.localeCompare(b)
+            return a.localeCompare(b);
         }
         if (this.sortSelect.value === 'sort-name-min'){
-            console.log(b.localeCompare(a))
-            return b.localeCompare(a)
+            return b.localeCompare(a);
         }
     }
 
@@ -40,13 +34,14 @@ export class FiltersSort {
     }
 
     renderButtons() {
+
         const titleSortControls = document.createElement('div');
         titleSortControls.className = 'title-controls';
         titleSortControls.innerText = 'Сортировка';
         filtersSort.appendChild(titleSortControls);
 
-       this.sortSelect = document.createElement('select');
-       this.sortSelect.className = 'sort-select';
+        this.sortSelect = document.createElement('select');
+        this.sortSelect.className = 'sort-select';
         titleSortControls.appendChild(this.sortSelect);
 
         this.sortSelect.options[0] = new Option ( 'По названию от «А» до «Я»', 'sort-name-max',  true, false);
@@ -65,30 +60,6 @@ export class FiltersSort {
 
     sortToys (a: HTMLOptionElement) {
         cardContauner.innerHTML = '';
-        console.log('sssss', a.value, this.sortSelect.value);
-
-        // if (this.selectorSort.size === 0){
-        //     this.selectorSort.add(a.value); 
-        // } else
-        // if (this.selectorSort.size > 0){
-        //     this.selectorSort.clear() ; 
-        //     this.selectorSort.add(a.value);
-        // }
-        //         if (a.value === 'sort-name-max'){
-        //     cards.sort((a, b) => a.name.localeCompare(b.name)).forEach(item => cardContauner.appendChild(item.card));
-        // }
-        // if (a.value === 'sort-name-min'){
-        //     cards.sort((a, b) => b.name.localeCompare(a.name)).forEach(item => cardContauner.appendChild(item.card));
-        // }
-        // if (a.value === 'sort-count-max'){
-        //     cards.sort((a, b) => a.count.localeCompare(b.count, undefined, {numeric: true})).forEach(item => cardContauner.appendChild(item.card));
-        // }
-
-        // if (a.value === 'sort-count-min'){
-        //     cards.sort((a, b) => b.count.localeCompare(a.count, undefined, {numeric: true})).forEach(item => cardContauner.appendChild(item.card));
-        // }
         this.filterCards();
     }
-
-
 }
