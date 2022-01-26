1d2e6812061a53efe1ab53eb3336e4d3a3c7d791
Olga <klokova.plus@gmail.com>
2022-01-26 14:17:32 +0100

    fix: tree -change img

--- js/tree.ts
+++ js/tree.ts
@@ -1,5 +1,4 @@
 import {containTree} from './containerTree';
-
 const favoritesMenu = document.querySelector('.favorites-menu');
 
 export class TreeCard {
@@ -24,25 +23,10 @@ export class TreeCard {
             treeContainers.appendChild(this.tree);
             this.tree.onclick = this.checkTree.bind(this, (i + 1));
         }
-
-        // const map = document.createElement('map');
-        // map.setAttribute('name', 'tree-map' );
-        // const area = document.createElement('area');
-        // area.setAttribute('coords', '365,699,189,706,113,683,31,608,2,555,2,539,18,437,73,351,106,224,161,134,243,-1,306,75,353,144,399,221,424,359,452,459,496,550,444,664')
-        // area.setAttribute('shape', 'poly');
-        // map.appendChild(area);
-        // containTree.treeContainer.appendChild(map);
-
-        // this.treeImg = new Image();
-        // this.treeImg.className = "christmas-tree";
-        // this.treeImg.src = `../assets/tree/2.webp`;
-        // this.treeImg.setAttribute('usemap', '#tree-map')
-        // this.treeImg.setAttribute('alt', 'tree')
-        // containTree.treeContainer.appendChild(this.treeImg);
     }
 
     checkTree(i: number) {
-        this.treeImg.src = `../assets/tree/${i}.webp`;
+        containTree.treeImg.src= `../assets/tree/${i}.webp`;
     }
 }
 
